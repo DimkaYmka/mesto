43e4f9c189174178cba54f741e563cbcd3596151
DimkaYmka <mungalov00@icloud.com>
2023-01-18 22:18:28 +0300

    check

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let popup = document.querySelector('.popup'); 
 let popupContainer = document.querySelector('.popup__container'); 
-let openPopupButton = document.querySelector('.open-popup'); // Кнопки для показа окна
-let closePopupButton = document.querySelector('.close-popup');
+let openPopupButton = document.querySelector('.profile__edit-button'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.popup__close-popup');
 
 let nameInfo = document.querySelector('.profile__name');// Воспользуйтесь инструментом .querySelector()
 let jobInfo = document.querySelector('.profile__text');// Воспользуйтесь инструментом .querySelector()
@@ -9,25 +9,25 @@ let inputName = document.querySelector('.popup__input_name');
 let inputInfo = document.querySelector('.popup__input_info');
 
 function closePopup() {
-  popup.classList.remove('active');
+  popup.classList.remove('popup_opened');
 }
 
 openPopupButton.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
       /*e.preventDefault();*/// Предотвращаем дефолтное поведение браузера
       inputName.value = nameInfo.textContent;
       inputInfo.value = jobInfo.textContent;
-      popup.classList.add('active'); // Добавляем класс 'active' для фона
-      popupContainer.classList.add('active'); // И для самого окна
+      popup.classList.add('popup_opened'); // Добавляем класс 'active' для фона
+     // popupContainer.classList.add('popup_opened'); // И для самого окна
   })
 
   closePopupButton.addEventListener('click',closePopup) ;
 
-document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-  if(e.target === popup) { // Если цель клика - фот, то:
-    popup.classList.remove('active'); // Убираем активный класс с фона
-    popupContainer.classList.remove('active'); // И с окна
-  }
-});
+//document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+ // if(e.target === popup) { // Если цель клика - фот, то:
+ //   popup.classList.remove('popup_opened'); // Убираем активный класс с фона
+  //  popupContainer.classList.remove('popup_opened'); // И с окна
+//   }
+//   });
 
 
 popupContainer.addEventListener('submit', handleFormSubmit); 
